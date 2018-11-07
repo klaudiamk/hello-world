d55e15b19a82251a287019f583f6c6d53301217f
Klaudia Kedra <klaudiak9019@gmail.com>
2018-11-07 10:49:23 -0500

    added javascript

---
+++ file.js
@@ -0,0 +1,11 @@
+function sayBrowser () {
+  var p = document.getElementById ("hello");
+  p.innerHTML = "Hello, World!";
+}
+function sayPopup () {
+  alert("Hello, World!")
+}
+function sayConsole () {
+  var p =document.getElementById ()
+  console.log ("Hello, World!")
+}
