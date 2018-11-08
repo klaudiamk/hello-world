a3c499e5fd100a11ed3969484a183a9e92daf840
Klaudia K <43784749+klaudiamk@users.noreply.github.com>
2018-11-07 19:47:58 -0500

    update file.js

--- file.js
+++ file.js
@@ -9,3 +9,11 @@ function sayConsole () {
   var p =document.getElementById ()
   console.log ("Hello, World!")
 }
+function sayStranger () {
+  var person = prompt ("Please enter your name:", "Your Name")
+  if (person == null || person == "") {
+        txt = "User cancelled the prompt.";
+    } else {
+        txt = "Hello " + person + "!";
+    }
+}
